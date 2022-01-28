8b5012692b416d299175a66de0495d1251dab861
cnlimiter <cnlimiter@163.com>
2022-01-28 22:23:28 +0800

    fix more bugs

--- src/main/java/committee/nova/plg/common/tiles/PlgTileEntity.java
+++ src/main/java/committee/nova/plg/common/tiles/PlgTileEntity.java
@@ -5,13 +5,21 @@ import committee.nova.plg.common.blocks.PlgBlock;
 import committee.nova.plg.common.blocks.PlgType;
 import committee.nova.plg.init.ModTileEntities;
 import committee.nova.plg.utils.energy.CachedEnergyStorage;
+import committee.nova.plg.utils.energy.PLGEnergyStorage;
 import net.minecraft.block.BlockState;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
 import net.minecraft.world.LightType;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -20,34 +28,35 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Date: 2022/1/28 12:30
  * Version: 1.0
  */
-public class PlgTileEntity extends BaseTileEntity {
+public class PlgTileEntity extends TileEntity implements ITickableTileEntity {
 
 
-
-
-
-    /** 数据 */
-    public double maxEnergyOutput;
+    private final PLGEnergyStorage energyStorage = createEnergy();
+    private final LazyOptional<PLGEnergyStorage> energy = LazyOptional.of(() -> energyStorage);
+    private double maxOutput;
 
     public PlgTileEntity() {
         super(ModTileEntities.PLG);
     }
 
-
-    public PlgType getPlgType() {
-        return ((PlgBlock)getBlockState().getBlock()).getType();
+    public PlgType getPlgType(){
+        return  ((PlgBlock)this.getBlockState().getBlock()).getType();
     }
 
+    @Override
+    public void setRemoved() {
+        super.setRemoved();
+        energy.invalidate();
+    }
 
     @Override
     public void tick() {
         final PlgType plgType = getPlgType();
-
-        maxEnergyOutput = plgType.getPower().getMaxProduction();
+        maxOutput = getPlgType().getPower().getMaxProduction();
 
         if (level != null && level.dimensionType().hasSkyLight()) {
             final int light = level.getBrightness(LightType.SKY, worldPosition.above()) - level.getSkyDarken();
-            insertEnergy(maxEnergyOutput * (int) ((light + 1) / 16F), false);
+            energyStorage.addEnergy((int) (maxOutput * (int) ((light + 1) / 16F)));
             setChanged();
         }
 
@@ -61,7 +70,7 @@ public class PlgTileEntity extends BaseTileEntity {
     }
 
     private void sendOutPower() {
-        final AtomicInteger capacity = new AtomicInteger((int) getEnergy());
+        final AtomicInteger capacity = new AtomicInteger(energyStorage.getEnergyStored());
         if (capacity.get() > 0) {
             final Direction[] directions = Direction.values();
             for (Direction direction : directions) {
@@ -70,9 +79,9 @@ public class PlgTileEntity extends BaseTileEntity {
                     final boolean doContinue = te.getCapability(CapabilityEnergy.ENERGY, direction.getOpposite()).map(handler ->
                             {
                                 if (handler.canReceive()) {
-                                    final int received = handler.receiveEnergy((int) Math.min(capacity.get(), maxEnergyOutput), false);
+                                    final int received = handler.receiveEnergy((int) Math.min(capacity.get(), maxOutput), false);
                                     capacity.addAndGet(-received);
-                                    consumeEnergy(received);
+                                    energyStorage.consumeEnergy(received);
                                     setChanged();
                                     return capacity.get() > 0;
                                 } else {
@@ -87,6 +96,29 @@ public class PlgTileEntity extends BaseTileEntity {
             }
         }
     }
+
+    private PLGEnergyStorage createEnergy() {
+        return new PLGEnergyStorage((int) maxOutput);
+    }
+
+    @Override
+    public CompoundNBT save(CompoundNBT tag) {
+        energy.ifPresent(h -> h.serializeNBT(tag));
+        return super.save(tag);
+    }
+
+    @Override
+    public void load(BlockState p_230337_1_, CompoundNBT tag) {
+        energy.ifPresent(h -> h.deserializeNBT(tag));
+        super.load(p_230337_1_, tag);
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        return energy.cast();    }
+
+
 
 
 
